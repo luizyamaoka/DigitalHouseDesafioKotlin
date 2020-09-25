c7c0ec423ae398b6d4d891c102a0ecefa5fd7da0
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-09-25 20:28:30 -0300

    parte a: classe aluno

--- src/br/com/digitalhouse/Principal.kt
+++ src/br/com/digitalhouse/Principal.kt
@@ -1,5 +1,16 @@
 package br.com.digitalhouse
 
 fun main() {
-    println("Main")
+
+    val aluno1 = Aluno(1, "Arthur", "Azevedo")
+    val aluno2 = Aluno(2, "Bianca", "Brasil")
+    val aluno3 = Aluno(3, "Carlos", "Carvalho")
+    val aluno4 = Aluno(4, "Danilo", "Dante")
+    val aluno5 = Aluno(5, "Elisa", "Eleonora")
+
+    val aluno6 = Aluno(5, "t", "t")
+
+    println(aluno5 == aluno6)
+    println(aluno5 == aluno1)
+
 }
