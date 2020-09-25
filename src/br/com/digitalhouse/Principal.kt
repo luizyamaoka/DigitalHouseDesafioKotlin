4763ab31324cb4bc20f0547176ac5c05b9dc3e59
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-09-25 20:39:55 -0300

    parte c: professor

--- src/br/com/digitalhouse/Principal.kt
+++ src/br/com/digitalhouse/Principal.kt
@@ -10,15 +10,28 @@ fun main() {
 
     val aluno6 = Aluno(5, "t", "t")
 
-    println(aluno5 == aluno6)
-    println(aluno5 == aluno1)
+    println("\n# Aluno")
+    println("Deveria ser true: ${aluno5 == aluno6}")
+    println("Deveria ser false: ${aluno5 == aluno1}")
 
     val curso1 = Curso(20001, "Full Stack")
     val curso2 = Curso(20002, "Android")
 
     val curso3 = Curso(20002, "teste")
 
-    println(curso1 == curso2)
-    println(curso3 == curso2)
+    println("\n# Curso")
+    println("Deveria ser false: ${curso1 == curso2}")
+    println("Deveria ser true: ${curso3 == curso2}")
+
+    val professor1 = Professor(1, "Felipe", "Fabregas", 0)
+    val professor2 = Professor(2, "Gabriela", "Giorgi", 0)
+    val professor3 = Professor(3, "Henrique", "Hana", 0)
+    val professor4 = Professor(4, "Indiana", "Iorq", 0)
+
+    val professor5 = Professor(3, "teste", "teste", 0)
+
+    println("\n# Professor")
+    println("Deveria ser false: ${professor1 == professor2}")
+    println("Deveria ser true: ${professor3 == professor5}")
 
 }
---
+++ src/br/com/digitalhouse/Professor.kt
@@ -0,0 +1,15 @@
+package br.com.digitalhouse
+
+class Professor(val codigo: Int, val nome: String, val sobrenome: String, val tempoDeCasa: Int) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Professor
+
+        if (codigo != other.codigo) return false
+
+        return true
+    }
+}
